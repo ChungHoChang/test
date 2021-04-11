6b2a8df74490117ab64dd4270ab194601aa10522
ChungHoChang <aho.cpc@gmail.com>
2021-04-11 23:17:40 +0800

    兩個變數相加

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 # print("helo")
 
-print("what is your name?")
-name = input("type your name ")
-print("hello, " + name)
+# print("what is your name?")
+# name = input("type your name ")
+# print("hello, " + name)
+
+print("First Number:")
+No1 = input()
+print("Second Number:")
+No2 = input()
+sum = No1 + No2
+print(sum)
