5a06534bc6629e3568d4e4020ad9822af91586d6
ChungHoChang <aho.cpc@gmail.com>
2021-04-14 22:19:32 +0800

    測試布林值，並且實作挑戰題

---
+++ exercise2.py
@@ -0,0 +1,23 @@
+# print(type('Hello world'))
+# print(type(7))
+
+# print(type(True))
+# print(type(False))
+
+# print(type('True'))
+# print(type('False'))
+
+# print(bool('True'))
+# print(bool('False'))
+# print(bool(''))
+# print(bool(' '))
+# print(bool('0'))
+# print(bool('1'))
+# print(bool(0))
+# print(bool(1))
+# print(bool(-1))
+# print(bool(2))
+# print(bool('Hello world!'))
+
+print(1 + 1 == 3)
+print(1 + 1 == 2)
