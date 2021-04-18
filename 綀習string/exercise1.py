2f7d3c21bbb08ca96ceec563e94afd8f792cf760
ChungHoChang <aho.cpc@gmail.com>
2021-04-18 20:07:34 +0800

    字串練習 將if練習搬位置

---
+++ 綀習string/exercise1.py
@@ -0,0 +1,39 @@
+# str1st = 'A literal string'
+# str2nd = 'A literal string'
+# print(str2nd == str1st)
+
+# str3rd = 'A single quoted literal string with a " double quote'
+# str4th = "A double quoted literal string with a ' single quote"
+# print(str3rd)
+# print(str4th)
+
+# fifth_string = 'A single quoted literal string with an \' escaped single quote'
+# sixth_string = "A double quoted literal string with a \" double quote"
+# seventh_string = 'A literal string with a \n new line character'
+# eighth_string = 'A literal string with a \t tab character'
+
+# print(fifth_string)
+# print(sixth_string)
+# print(seventh_string)
+# print(eighth_string)
+
+# str9th = r"A literal string with a \n new line character printed raw"
+# print(str9th)
+
+# tenth_string = '''A literal string
+#     on more than one line
+#     sometimes known as a verbatim string'''
+# eleventh_string = """Another literal string
+#     on more than one line
+#     using double quotes"""
+
+# print(tenth_string)
+# print(eleventh_string)
+
+# first = 'Conrad'
+# second = 'Grant'
+# third = 'Bob'
+# print(first, second)
+# print(first, second, third)
+# print(first, second, third, sep='-')
+# print(first, second, third, sep='-', end='.')
