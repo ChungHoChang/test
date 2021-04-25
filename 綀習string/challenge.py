25a3e93091aaaa7b361bbad399417a719865089f
ChungHoChang <aho.cpc@gmail.com>
2021-04-25 20:46:40 +0800

    完成字串練習挑戰

--- 綀習string/challenge.py
+++ 綀習string/challenge.py
@@ -2,24 +2,19 @@ first_value = '  FIRST challenge         '
 second_value = '-  second challenge  -'
 third_value = 'tH IR D-C HALLE NGE'
 
+
 fourth_value = 'fourth'
 fifth_value = 'fifth'
 sixth_value = 'sixth'
 
+
 # First challenge
-
-
+print(' '*7 + first_value.lower().strip().capitalize())
 # Second challenge
-
-
+print(second_value.replace('-', '').strip().capitalize())
 # Third challenge
-
-
-print(first_value)
-print(second_value)
-print(third_value)
-
+print(' '*15 + third_value.replace(' ', '').replace('-', ' ').lower().capitalize())
 # Fourth challenge - use only the print() function (no f-strings)
-
-
+print(fourth_value, fifth_value, sixth_value, sep='#', end='!\n')
 # Fifth challenge - use only a single print() function.  Create tabs and new lines using f-strings.
+print(f'\t{fourth_value}\n\t{fifth_value}\n\t{sixth_value}\n')
